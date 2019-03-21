a9e20f21691f561445e98b4781ed11c6e5b7e2d6
Htet Wai Yan <htetwaiyanabcd@gmail.com>
2019-03-22 00:09:02 +0800

    add project files and readme

---
+++ BooksA/app/src/main/java/com/example/wingyi/booksa/BooksSearch.java
@@ -0,0 +1,13 @@
+package com.example.wingyi.booksa;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BooksSearch extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_books_search);
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# PROJECT DETAILS
+
+- built for admin users
+- admin users can browse books
+- admin users can manage book information
+- developed by a team which includes three members
